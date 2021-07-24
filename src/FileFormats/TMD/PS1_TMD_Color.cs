0e565e1bb1913747afc5367c05396d6259f6f86a
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-07-24 21:32:07 +0200

    Change TMD color to RGB777 (0-127)

--- src/FileFormats/TMD/PS1_TMD_Color.cs
+++ src/FileFormats/TMD/PS1_TMD_Color.cs
@@ -2,11 +2,11 @@
 {
     public class PS1_TMD_Color : BinarySerializable
     {
-        public RGB888Color Color { get; set; }
+        public RGB777Color Color { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
-            Color = s.SerializeObject<RGB888Color>(Color, name: nameof(Color));
+            Color = s.SerializeObject<RGB777Color>(Color, name: nameof(Color));
             s.SerializePadding(1);
         }
     }
