1b475520e5d9bc4461b63fd8c6319b25bfb9ef7b
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-08-18 17:42:30 +0200

    Add VRAMRegion

---
+++ src/VRAM/PS1_VRAMRegion.cs
@@ -0,0 +1,18 @@
+﻿namespace BinarySerializer.PS1
+{
+    public class PS1_VRAMRegion : BinarySerializable
+    {
+        public short XPos { get; set; }
+        public short YPos { get; set; }
+        public short Width { get; set; }
+        public short Height { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            XPos = s.Serialize<short>(XPos, name: nameof(XPos));
+            YPos = s.Serialize<short>(YPos, name: nameof(YPos));
+            Width = s.Serialize<short>(Width, name: nameof(Width));
+            Height = s.Serialize<short>(Height, name: nameof(Height));
+        }
+    }
+}
