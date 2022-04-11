6fc1c774738560a55ed1bbcb77d6db925f4650cb
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-04-11 08:56:10 +0200

    Serialize TMD colors as 888

--- src/FileFormats/TMD/PS1_TMD_Color.cs
+++ src/FileFormats/TMD/PS1_TMD_Color.cs
@@ -2,11 +2,12 @@
 {
     public class PS1_TMD_Color : BinarySerializable
     {
-        public RGB777Color Color { get; set; }
+        // Note: The color values have a range between 0-2 rather than 0-1
+        public RGB888Color Color { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
-            Color = s.SerializeObject<RGB777Color>(Color, name: nameof(Color));
+            Color = s.SerializeObject<RGB888Color>(Color, name: nameof(Color));
             s.SerializePadding(1);
         }
     }
