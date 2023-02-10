91c3418a733677a8917515cced0ebcf10b113aab
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-02-10 10:22:16 +0100

    Add CdlFILE and CdlLOC

---
+++ src/Psyq/CdlFILE.cs
@@ -0,0 +1,31 @@
+﻿namespace BinarySerializer.PS1.Psyq
+{
+    public class CdlFILE : BinarySerializable
+    {
+        public bool Pre_HasFileName { get; set; } = true; // Older Psyq versions don't seem to include the name
+
+        /// <summary>
+        /// The file position on the disc
+        /// </summary>
+        public CdlLOC Pos { get; set; }
+        
+        /// <summary>
+        /// The file size
+        /// </summary>
+        public uint Size { get; set; }
+        
+        /// <summary>
+        /// The file name. This does not contain the directory path.
+        /// </summary>
+        public string Name { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            Pos = s.SerializeObject<CdlLOC>(Pos, name: nameof(Pos));
+            Size = s.Serialize<uint>(Size, name: nameof(Size));
+
+            if (Pre_HasFileName)
+                Name = s.SerializeString(Name, length: 16, name: nameof(Name));
+        }
+    }
+}
---
+++ src/Psyq/CdlLOC.cs
@@ -0,0 +1,63 @@
+﻿namespace BinarySerializer.PS1.Psyq
+{
+    public class CdlLOC : BinarySerializable
+    {
+        public byte Minute { get; set; }
+        public byte Second { get; set; }
+        public byte Sector { get; set; }
+        public byte Track { get; set; } // Unused
+
+        protected int MinuteValue
+        {
+            get => FromBCD(Minute);
+            set => Minute = ToBCD(value);
+        }
+        protected int SecondValue
+        {
+            get => FromBCD(Second);
+            set => Second = ToBCD(value);
+        }
+        protected int SectorValue
+        {
+            get => FromBCD(Sector);
+            set => Sector = ToBCD(value);
+        }
+
+        public int LBA
+        {
+            get => (SectorValue + (SecondValue * 75) + (MinuteValue * 60 * 75)) - 150;
+            set
+            {
+                var tmp = value + 150;
+                SectorValue = (byte)(tmp % 75);
+                SecondValue = (byte)((tmp / 75) % 60);
+                MinuteValue = (byte)(tmp / 75 / 60);
+            }
+        }
+
+        protected int FromBCD(byte bcd)
+        {
+            var result = 0;
+            result *= 100;
+            result += (10 * (bcd >> 4));
+            result += bcd & 0xf;
+            return result;
+        }
+
+        protected byte ToBCD(int value)
+        {
+            int tens = value / 10;
+            int units = value % 10;
+            return (byte)((tens << 4) | units);
+        }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            Minute = s.Serialize<byte>(Minute, name: nameof(Minute));
+            Second = s.Serialize<byte>(Second, name: nameof(Second));
+            Sector = s.Serialize<byte>(Sector, name: nameof(Sector));
+            Track = s.Serialize<byte>(Track, name: nameof(Track));
+            s.Log("LBA: 0x{0:X8}", LBA);
+        }
+    }
+}
